9af0a222703bc2d151975919398cefd4e2b8e169
Alex Jercan <jercan_alex27@yahoo.com>
2025-01-23 13:59:50 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "A basic flake for my Bevy Game";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: (
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config = {
+          allowUnfree = true;
+        };
+      };
+      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+    in {
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = manifest.name;
+        version = manifest.version;
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+      };
+
+      devShells.default = pkgs.mkShell rec {
+        nativeBuildInputs = with pkgs; [
+          # Tools
+          blender
+          # Rust Compiler
+          cargo
+          rustc
+          rustfmt
+          clippy
+          pkg-config
+          llvmPackages.bintools
+        ];
+
+        buildInputs = with pkgs; [
+          udev
+          alsa-lib-with-plugins
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr # To use the x11 feature
+          libxkbcommon
+          wayland # To use the wayland feature
+        ];
+
+        RUST_BACKTRACE = 1;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    })
+  );
+}
